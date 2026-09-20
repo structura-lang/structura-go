e016ee944583324d094a51c0b004d1dd69a5dedd
Karotte128 <karotte128@outlook.de>
2026-09-20 12:35:45 +0200

    undo debug change

--- interpreter/operations.go
+++ interpreter/operations.go
@@ -36,7 +36,7 @@ func (o operation) evaluate(pv parentVariables) error {
 			return fmt.Errorf("`arguments/output_to` field is not a string!")
 		}
 
-		pv.Variables[outputTo].(map[string]any)["out"] = value
+		pv.Variables[outputTo] = value
 
 		return nil
 
